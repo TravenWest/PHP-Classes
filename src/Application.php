579fdb1e3cb0dd6d5912bb4615876e9b3b11fdbb
Traven <traven.west@gmail.com>
2016-02-08 01:45:18 -0500

    Revert "Added several new classes"
    This reverts commit aa2e993a6ca7fc4ca58ee566a8dc581a4a7359b5.

--- src/Application.php
+++ src/Application.php
@@ -1,397 +1,51 @@
 <?php
 
-/**
- *   This is the base application class for a web application. We are going
- *   to be setting up the environment as necessary and this class acts like
- *   the registry for us. It can also be a broker for the Autoloader.
+/**  
+ *   Project:    XML Phonebook
+ *   Package:    Application.php
+ *   Authors:    Traven, Truvis
+ *   Version:    1.0.4
+ *   License:    BSD Clause 3, New or Revisted License
  *   
- *   Project:  QuadFramework
- *   Package:  Application.php
- *   Authors:  Traven West <me@travenwest.com>
- *   Version:  1.0.1
+ *   This class is going to handle generalized functions which the full web 
+ *   can access and used. The reasoning behind this setup is to organize the
+ *   source code into logical thinking pieces. Also it helps with the actual
+ *   page construction and overall feel of a properly coded application.
  */
  
- 
-class QuadFramework_Application extends Zend_Registry
+class Application
 {
-	
-	const URL_ID_DELIMITER = '.';
-    
-    
-    public static $version   = '1.0.4';
-	public static $versionID = 01000407; // AABBCCDD
-
-    
-    public static $jQueryVersion = '1.4.4';
-    
-    
-    protected $_rootDir = '.';
-    
-    
-    protected $_initialized = false;
-    
-    
-    protected static $_handlePhpError = true;
-    
-    
-    protected static $_debug;
-    
-    
-    protected static $_classCache = array();
-    
-    
-    public static $time = 0;
-    
-    
-    public static $host = 'localhost';
-    
-    
-    public static $secure = false;
-    
-    
-    public static $integerSentinel = '{{sentinel}}';
-    
-    
-    public static $externalDataPath = 'data';
-    
-    
-    public static $externalDataUrl = 'data';
-    
-    
-    public static $javaScriptUrl = 'js';
-    
-    
-	public function beginApplication($configDir = '.', $rootDir = '.', $loadDefaultData = true)
-	{
-		if ($this->_initialized)
-		{
-			return;
-		}
-
-		if (function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc())
-		{
-			self::undoMagicQuotes($_GET);
-			self::undoMagicQuotes($_POST);
-			self::undoMagicQuotes($_COOKIE);
-			self::undoMagicQuotes($_REQUEST);
-		}
-		if (function_exists('get_magic_quotes_runtime') && get_magic_quotes_runtime())
-		{
-			@set_magic_quotes_runtime(false);
-		}
-
-		@ini_set('memory_limit', 128 * 1024 * 1024);
-		ignore_user_abort(true);
-
-		@ini_set('output_buffering', false);
-
-		//  See http://bugs.php.net/bug.php?id=36514
-		if (!@ini_get('output_handler')) while (@ob_end_clean());
-
-		error_reporting(E_ALL | E_STRICT & ~8192);
-		set_error_handler(array('QuadFramework_Application', 'handlePhpError'));
-		set_exception_handler(array('QuadFramework_Application', 'handleException'));
-
-		//  @ini_set('pcre.backtrack_limit', 1000000);
-
-		date_default_timezone_set('UTC');
-
-		self::$time = time();
-
-		self::$host = (empty($_SERVER['HTTP_HOST']) ? '' : $_SERVER['HTTP_HOST']);
-
-		self::$secure = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on');
-
-		$this->_configDir = $configDir;
-		$this->_rootDir = $rootDir;
-		$this->addLazyLoader('requestPaths', array($this, 'loadRequestPaths'));
-
-		if ($loadDefaultData)
-		{
-			$this->loadDefaultData();
-		}
-
-		$this->_initialized = true;
-	}
-    
-    
-    public static function initialize($configDir = '.', $rootDir = '.', $loadDefaultData = true)
-	{
-		self::setClassName(__CLASS__);
-		self::getInstance()->beginApplication($configDir, $rootDir, $loadDefaultData);
-	}
-    
-    
-    public static function handlePhpError($errorType, $errorString, $file, $line)
-	{
-		if (!self::$_handlePhpError)
-		{
-			return false;
-		}
-
-		if ($errorType & error_reporting())
-		{
-			throw new ErrorException($errorString, 0, $errorType, $file, $line);
-		}
-	}
-    
-    
-    public static function disablePhpErrorHandler()
-	{
-		self::$_handlePhpError = false;
-	}
-    
-    
-    public static function enablePhpErrorHandler()
-	{
-		self::$_handlePhpError = true;
-	}
-    
-    
-    public static function handleException(Exception $error)
-	{
-		QuadFramework_Error::logException($error);
-		QuadFramework_Error::unexpectedException($error);
-	}
-    
-    
-    public static function debugMode()
-	{
-		return self::$_debug;
-	}
-    
-    
-    public static function setDebugMode($debug)
-	{
-		self::$_debug = (boolean)$debug;
-
-		if (self::$_debug)
-		{
-			@ini_set('display_errors', true);
-		}
-	}
-    
-    
-    public function getConfigDir()
-	{
-		return $this->_configDir;
-	}
-    
-    
-    public function getRootDir()
-	{
-		return $this->_rootDir;
-	}
-    
-    
-    public function loadDb(Zend_Config $dbConfig)
-	{
-		$db = Zend_Db::factory($dbConfig->adapter,
-			array(
-				'host' => $dbConfig->host,
-				'port' => $dbConfig->port,
-				'username' => $dbConfig->username,
-				'password' => $dbConfig->password,
-				'dbname' => $dbConfig->dbname,
-				'charset' => 'utf8'
-			)
-		);
-
-		switch (get_class($db))
-		{
-			case 'Zend_Db_Adapter_Mysqli':
-				$db->getConnection()->query("SET @@session.sql_mode='STRICT_ALL_TABLES'");
-				break;
-			case 'Zend_Db_Adapter_Pdo_Mysql':
-				$db->getConnection()->exec("SET @@session.sql_mode='STRICT_ALL_TABLES'");
-				break;
-		}
-
-		if (self::debugMode())
-		{
-			$db->setProfiler(true);
-		}
-
-		return $db;
-	}
-    
-    
-    public function loadCache(Zend_Config $cacheConfig)
-	{
-		if (!$cacheConfig->enabled)
-		{
-			return false;
-		}
-
-		return Zend_Cache::factory(
-		    $cacheConfig->frontend,
-		    $cacheConfig->backend,
-		    $cacheConfig->frontendOptions->toArray(),
-		    $cacheConfig->backendOptions->toArray()
-		);
-	}
-    
-    
-    public function loadRequestPaths()
-	{
-		return self::getRequestPaths(new Zend_Controller_Request_Http());
-	}
-    
-    
-    public static function getRequestPaths(Zend_Controller_Request_Http $request)
-	{
-		$basePath = $request->getBasePath();
-		if ($basePath === '' || substr($basePath, -1) != '/')
-		{
-			$basePath .= '/';
-		}
-
-		$host = $request->getServer('HTTP_HOST');
-		if (!$host)
-		{
-			$host = $request->getServer('SERVER_NAME');
-			$serverPort = intval($request->getServer('SERVER_PORT'));
-			if ($serverPort && $serverPort != 80 && $serverPort != 443)
-			{
-				$host .= ':' . $serverPort;
-			}
-		}
-
-		$protocol = ($request->isSecure() ? 'https' : 'http');
-
-		$requestUri = $request->getRequestUri();
-
-		return array(
-			'basePath' => $basePath,
-			'host' => $host,
-			'protocol' => $protocol,
-			'fullBasePath' => $protocol . '://' . $host . $basePath,
-			'requestUri' => $requestUri,
-			'fullUri' => $protocol . '://' . $host . $requestUri
-		);
-	}
-    
-    
-    public static function get($index)
-	{
-		$instance = self::getInstance();
-
-		if (!$instance->offsetExists($index))
-		{
-            throw new Zend_Exception("No entry is registered for key '$index'");
-		}
-
-		return $instance->offsetGet($index);
-	}
-    
-    
-    public static function getWithFallback($index, $callback, array $args = array())
-	{
-		if (self::isRegistered($index))
-		{
-			return self::get($index);
-		}
-		else
-		{
-			$result = call_user_func_array($callback, $args);
-			self::set($index, $result);
-			return $result;
-		}
-	}
-    
-    
-    public static function autoload($class)
-	{
-		return QuadFramework_Autoloader::getInstance()->autoload($class);
-	}
-    
-    
-    public static function undoMagicQuotes(&$array, $depth = 0)
-	{
-		if ($depth > 10 || !is_array($array))
-		{
-			return;
-		}
-
-		foreach ($array AS $key => $value)
-		{
-			if (is_array($value))
-			{
-				self::undoMagicQuotes($array[$key], $depth + 1);
-			}
-			else
-			{
-				$array[$key] = stripslashes($value);
-			}
-
-			if (is_string($key))
-			{
-				$new_key = stripslashes($key);
-				if ($new_key != $key)
-				{
-					$array[$new_key] = $array[$key];
-					unset($array[$key]);
-				}
-			}
-		}
-	}
-    
-    
-    public static function gzipContentIfSupported(&$content)
-	{
-		if (@ini_get('zlib.output_compression') || @ini_get('output_handler'))
-		{
-			return array();
-		}
-
-		if (!function_exists('gzencode') || empty($_SERVER['HTTP_ACCEPT_ENCODING']))
-		{
-			return array();
-		}
-
-		if (!is_string($content))
-		{
-			return array();
-		}
-
-		if (!self::get('config')->enableGzip)
-		{
-			return array();
-		}
-
-		$headers = array();
-
-		if (strpos($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') !== false)
-		{
-			$headers[] = array('Content-Encoding', 'gzip', true);
-			$headers[] = array('Vary', 'Accept-Encoding', false);
-
-			$content = gzencode($content, 1);
-		}
-
-		return $headers;
-	}
-    
-    
-    public static function generateRandomString($length)
-	{
-		while (strlen(self::$_randomData) < $length)
-		{
-			//  OpenSSL_Random_Pseudo_Bytes is **ridiculously** slow on windows
-			if (function_exists('openssl_random_pseudo_bytes') && substr(PHP_OS, 0, 3) != 'WIN')
-			{
-				self::$_randomData .= bin2hex(openssl_random_pseudo_bytes(max($length, 1024) / 2));
-			}
-			else
-			{
-				self::$_randomData .= md5(uniqid(mt_rand(), true));
-			}
-		}
-
-		$return = substr(self::$_randomData, 0, $length);
-		self::$_randomData = substr(self::$_randomData, $length);
-
-		return $return;
-	}
+    /**
+     *   I am not sure if we need this or not, but it may come in handy for us
+     *   when we are working on new feature releases of the web application. Integer
+     *   do know that havin a version number reference for the "SysAdmins" would
+     *   help with extending and version control of each new release.
+     *
+     *   Variable:  String
+     *   Returns:   Version Number and ID
+     */
+    public static $version   = "1.0.4";
+    public static $versionID = "01000004";  //  AABBCC0D (7 = stable, 5 = beta, 3 = alpha)
+    
+    
+    /**
+	 *   We need to define the path to the web application's root directory
+     *   because we can look for other directories within the root.
+	 *
+	 *   Variable:  String
+     *   Returns:   Root directory of the web application
+	 */
+	public static $rootDir = '.';
+    
+    
+    
+    /**
+     *   We need to get unix timestamp representing the current webserver date
+     *   and time. This is for getting dates of creation and "now" reference.
+	 *
+	 *   Variable:  Integer
+     *   Returns:   Current time in Unix 
+	 */
+	public static $time = 0;
     
 }
---
+++ src/Database.php
@@ -0,0 +1,161 @@
+<?php
+
+/**  
+ *   Project:    LearnPHP CMS
+ *   Package:    Database.php
+ *   Authors:    Traven, Truvis
+ *   Version:    1.0.4
+ *   License:    BSD Clause 3, New or Revisted License
+ *   
+ *   This class is going to hadle all database connections and queries for the
+ *   whole webh application. The functions prepare all of the queries before
+ *   they are executed. This is the core class to the web application!
+ */
+ 
+class Database
+{
+     
+     /**
+      *   These variables are going to allow us to connect to the MySQL
+      *   database which the web application requires. This information
+      *   should be protected and never shared outside of the class.
+      *
+      *   Variables:  String
+      *   Returns:    MNySQL database login information
+      */
+     protected $Hostname = '';
+     protected $Database = '';
+     protected $Username = '';
+     protected $Password = '';
+     
+     
+     /**
+      *   I am going to store the class instance as a variable for easier use
+      *   and to create SQL queries and functions. This should allow for the 
+      *   best security and prepared statements.
+      *   
+      *   Variables:  Instance
+      *   Returns:    The database/PDO class
+      */
+     protected $PDO;
+     
+     /**
+      *   We are going to allow the Database class to handle errors itself till
+      *   we create an error handling class. This is going to save a lot of 
+      *   time so we don't have to try to get the error handling to work.
+      *   
+      */
+     protected $error;
+     
+     /**
+      *   We are going to store the SQL statement as an object of the class so
+      *   we can protect it from outside classes if another class was to be
+      *   exploitable or vulnerable in the future. This is one of the safest
+      *   ways to handle SQL queries for "future proofing".
+      */
+     protected $statement;
+
+     
+     /**
+      *   We are going to make our database connection globally available to
+      *   the whole web application. This is going to allow us to insert SQL
+      *   queries directly into our the application.
+      *
+      *   Variables:  $PDO::%Function%
+      *   Returns:    Globally accessable database instance
+      */
+     public function __construct()
+     {
+          //  I'm going to turn the "heavy lifting" piece of the connection as
+          //  a simplized variable for our use of it.
+          $DNS = 'mysql:host=' . $this->hostname . ';dbname=' . $this->database;
+          
+          //  I am going to add some "properly coded" options to the use of the
+          //  database connection. We are going to make our connection be "on"
+          //  all of the time. Then if any errors decide to happen to us while
+          //  we are "running" or developing the web application, they are 
+          //  going to be handled "gracefully" over a ugly mess.
+           $options = array(
+            PDO::ATTR_PERSISTENT    => true,
+            PDO::ATTR_ERRMODE       => PDO::ERRMODE_EXCEPTION
+          );
+          
+          //  Now I am going to do the actual connection of the database within 
+          //  the class constructor so we gain global access to the connection.
+          try
+          {
+               $this->PDO = new PDO($DNS, $this->Username, $this->Password, $options);
+          }
+
+          catch(PDOException $error)
+          {
+               $this->error = $error->getMessage();
+          }
+     }
+     
+     
+     /**
+      *   I'm going to make our SQL query default to being a prepared statement
+      *   over allowing our developing to cause SQL injections. This is a major
+      *   security enhancing idea. In other words, all SQL statements are going
+      *   to be prepared to remove the worry of hackers doing SQL injections.
+      *   
+      *   Variables:  SQL statement 
+      *   Returns:    Prepared sql statement
+      */
+     public function query($query)
+     {
+          $this->statement = $this->PDO->prepare($query);
+     }
+     
+     
+     /**
+      *   This may be the most complex piece of the Database class that will
+      *   be used in all SQL statements. We are preparing the queries even
+      *   further to stop all exploition. We are using a switch statement to
+      *   allow the code/query choose the type of data which is going to be
+      *   returned to us from the four common types.
+      *   
+      *   Variables:  Types
+      *   Returns:    Binded SQL results
+      */
+     public function bind($param, $value, $type = null)
+     {
+          if (is_null($type)) 
+               {
+                    switch (true) 
+                    {
+                         case is_int($value):
+                         $type = PDO::PARAM_INT;
+                         break;
+                         
+                         case is_bool($value):
+                         $type = PDO::PARAM_BOOL;
+                         break;
+                         
+                         case is_null($value):
+                         $type = PDO::PARAM_NULL;
+                         break;
+                         
+                    default:
+                    $type = PDO::PARAM_STR;
+                    }
+               }
+          
+          $this->statement->bindValue($param, $value, $type);
+     
+     }
+     
+     /**
+      *   Now I can can create our function to execute our prepared the SQL
+      *   statements that we write in the web application. All of the work 
+      *   to prepare the statements is done, and now we need to actually
+      *   execute and return our results which we requested.
+      *   
+      *   Returns:    The query results
+      */
+     public function execute()
+     {
+          return $this->statement->execute();
+     }
+}
